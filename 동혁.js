72ceeecad7dbe653eee92bb1fdbce9e7fe5453b8
gur0601 <80812195+gur0601@users.noreply.github.com>
2021-10-01 12:08:16 +0900

    저장1

---
+++ 동혁.js
@@ -0,0 +1,9 @@
+"1부터 50의 자연수 중 짝수를 구하는 함수"
+let a= []
+    function number(num){
+        for(i=0; i<=50; i++){
+            if(num&2===0){
+        return  a.push       
+            }
+        } 
+        }
